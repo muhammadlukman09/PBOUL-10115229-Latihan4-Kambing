3d64520909e297732fc1b171ce11820b2cde0a4e
fakhri ramdani <muhammadlukman09@gmail.com>
2019-09-26 09:50:15 +0700

    project keempat

---
+++ src/pboul/pkg10115229/latihan4/kambing/PBOUL10115229Latihan4Kambing.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan4.kambing;
+
+/**
+ *
+ * @author Lukman
+ * NIM          : 10115229
+ * NAMA         : MUHAMMAD LUKMAN HAKIM
+ * HARITANGGAL  : JUM'AT, 20 SEPTEMBER 2019
+ * LOG
+ */
+public class PBOUL10115229Latihan4Kambing {
+
+    /**
+     * @param args the command line arguments
+     */
+            
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+            public class Kambing {
+            
+            public void tambahKambing(){
+            // Deklarasi variabel lokal
+            int jumlahKambing = 0;
+            
+            jumlahKambing = jumlahKambing + 5;
+            System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+            }
+    public static void main(String[] args){
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+      
+            
+        }
+    }
+        
+}
